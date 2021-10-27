7b4f1b14c701043bc60ff1f87ac3ffbf195ac72c
Dan Hersam <dan@hersam.com>
2021-10-27 15:38:13 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Text-based calculator with history
+
+![](text-calculator-thumbnail.png)
+
+A simple, text-based calculator to do quick arithmetic. Aims to be a simplified, online version of [SpeedCrunch](https://speedcrunch.org/) or PowerToys PowerCalc.
+
+## Usage
+
+* Enter expressions and see the results without even hitting Enter.
+* In the expression field:
+    * Type up arrow to show the most recent expression.
+    * Type down arrow or Esc to clear the current expression.
+    * Type an operator to use the previous answer (e.g. +2 will add 2 to the previous answer).
+    * Type 'c' or hit the Clear button to clear all of the fields.
+
+[Try it out here](https://calc.danhersam.com/).
+
+## Credits
+
+* Built with [Math.js](https://mathjs.org/), [Vue.js](https://vuejs.org/) and [Bulma](https://bulma.io/)
+* Favicon from [Icons8](https://icons8.com/icon/23154/calculator)
+
+## Updating dependencies
+
+* Vue - Download the [latest production version](https://vuejs.org/js/vue.min.js) and rename it with the version.
+* Math.js - Download the [latest version](https://unpkg.com/mathjs@latest/lib/browser/math.js) and rename it with the version.
---
+++ public/app.js
@@ -0,0 +1,104 @@
+function startsWithMathOperator(originalExpression) {
+  const operators = ['+', '-', '*', '/', '^', '!'];
+  const expression = originalExpression.trim();
+
+  if (expression === '') {
+    return false;
+  }
+
+  return operators.includes(expression[0]);
+}
+
+function isValidAnswer(ans) {
+  if (typeof(ans) === 'undefined' || ans === '') {
+    return false;
+  }
+
+  return true;
+}
+
+let historyLog;
+let lastHistoryLogScrollHeight;
+
+new Vue({
+  el: '#app',
+  data: {
+    expression: '',
+    lastExpression: '',
+    ans: '',
+    resultHistory: [],
+    expressionHasError: false,
+  },
+
+  mounted: function () {
+    historyLog = document.getElementById('historyLog');
+    lastHistoryLogScrollHeight = historyLog.scrollHeight;
+  },
+
+  methods: {
+    addToHistory: function () {
+      if (this.expressionHasError || this.expression === '') {
+        return;
+      }
+
+      this.resultHistory.push({ expression: this.expression, answer: this.result });
+      this.lastExpression = this.expression;
+      this.ans = this.result;
+      this.expression = '';
+    },
+
+    showLastExpression: function () {
+      if (this.lastExpression === '') {
+        return;
+      }
+
+      this.expression = this.lastExpression;
+    },
+
+    clearExpression: function () {
+      this.expression = '';
+    },
+
+    clearAll: function () {
+      this.expression = '';
+      this.resultHistory = [];
+      this.ans = '';
+
+      document.getElementById('expression').focus();
+    },
+  },
+
+  computed: {
+    result: function () {
+      try {
+        if (this.expression === '') {
+          return '';
+        }
+
+        if (startsWithMathOperator(this.expression) && isValidAnswer(this.ans)) {
+          this.expression = `${this.ans} ${this.expression}`;
+        }
+
+        const answer = math.evaluate(this.expression);
+
+        if (answer !== undefined) {
+          this.expressionHasError = false;
+          return answer;
+        }
+
+        return '';
+      } catch (err) {
+        this.expressionHasError = true;
+      }
+    },
+  },
+
+  updated: function () {
+    this.$nextTick(function () {
+      if (historyLog.scrollHeight > lastHistoryLogScrollHeight) {
+        historyLog.scrollTop = historyLog.scrollHeight;
+        lastHistoryLogScrollHeight = historyLog.scrollHeight;
+      }
+    });
+  },
+});
