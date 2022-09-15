e36be57014c1165c0ded782caff4ce385404a2a8
Dan Hersam <142353+jaden@users.noreply.github.com>
2022-09-15 11:21:08 -0400

    Strip commas from incoming values
    Reduce precision slightly to avoid .000000001 issues

--- public/app.js
+++ public/app.js
@@ -144,13 +144,16 @@ new Vue({
           return '';
         }
 
+        // Remove commas (math library can't calculate with them)
+        this.expression = this.expression.replaceAll(',', '');
+
         const expressionResults = getExpressionToEvaluate(this.expression, this.ans);
 
         this.expression = expressionResults.expression;
 
         const originalAnswer = math.evaluate(expressionResults.expressionToEvaluate);
         const answer = math.format(originalAnswer, {
-          precision: 14, // Prevent round-off errors in output
+          precision: 13, // Prevent round-off errors in output
           upperExp: 12, // Exponent at which to start using scientific notation
         });
 
